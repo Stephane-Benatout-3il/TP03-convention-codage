c4db622a2269c70b2d11dba3bb2f3e23eae0dd5d
StephaneBenatout <benatous#3il.fr>
2024-11-19 16:09:26 +0100

    exo 2 fait

--- Exercice 1/exo1.java
+++ Exercice 1/exo1.java
@@ -1,15 +1,21 @@
-//Création de la class Animal avec ses procédures , fonction et varriables
+/**
+ * Création de la class Animal avec ses procédures , fonction et varriables
+ * **/
 public class Animal{
 	public String nom;
 	public int age;
 	
-	//procédure qui affiche à la console "Je suis un animal."
-	public void Parler(){
+	/**
+	 * procédure qui affiche à la console "Je suis un animal."
+	 * **/
+	public void parler(){
 		System.out.println("Je suis un animal.");
 	}
 	
-	//prcédure qui affiche les infos de l'animal
-	public void AfficherInfo(){
+	/**
+	 * prcédure qui affiche les infos de l'animal
+	 * **/
+	public void afficherInfo(){
 		System.out.println("Nom: "+nom+", Âge: "+age+" ans");
 	}
 }
--- Exercice 2/exo2.java
+++ Exercice 2/exo2.java
@@ -1,13 +1,58 @@
-public class Veterinaire {
-    public void soignerAnimal(String type, String nom) {
-        if (type.equals("Chien")) {
-            System.out.println("Soins pour le chien " + nom);
-            System.out.println("Examen des pattes");
-            System.out.println("Injection de vaccin");
-        } else if (type.equals("Chat")) {
-            System.out.println("Soins pour le chat " + nom);
-            System.out.println("Examen des griffes");
-            System.out.println("Nettoyage des oreilles");
+public class Veterinaire{
+    public void soignerAnimal(String type, String nom){
+        if(type.equals("Chien")){
+            soignerChien();
+            examenPattes();
+            vaccin();
+        }
+        else if(type.equals("Chat")){
+            soignerChat();
+            examenGriffes();
+            nettoyageOreilles();
         }
     }
+
+    /***
+     * procédure du soin des chiens
+     * **/
+    private void soignerChien(){
+        System.out.println("Soins pour le chien " + nom);
+    }
+
+    /***
+     * procédure d'axamen des pattes
+     * **/
+    private void examenPattes(){
+        System.out.println("Examen des pattes");
+    }
+
+    /**
+     * procédure d'injection de vaccin
+     * **/
+    private void vaccin(){
+        System.out.println("Injection de vaccin");
+    }
+    
+
+    /**
+     * procédure du soin des chats
+     * **/
+    private void soignerChat(){
+        System.out.println("Soins pour le chat " + nom);
+    }
+
+    /**
+     * procédure d'examen des Griffes
+     * **/
+    private void examenGriffes(){
+        System.out.println("Examen des griffes");
+    }
+
+
+    /**
+     * procédure du nettoyage des oreilles
+     * **/
+     private nettoyageOreilles(){
+        System.out.println("Nettoyage des oreilles");
+     }
 }
