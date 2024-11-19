3a7686dc80122f7c318182f83ba6ee6550ac1051
StephaneBenatout <benatous#3il.fr>
2024-11-19 15:51:32 +0100

    exo 1 fait

--- Exercice 1/exo1.java
+++ Exercice 1/exo1.java
@@ -1,11 +1,14 @@
-public class animal{
+//Création de la class Animal avec ses procédures , fonction et varriables
+public class Animal{
 	public String nom;
 	public int age;
 	
-	public void parler(){
+	//procédure qui affiche à la console "Je suis un animal."
+	public void Parler(){
 		System.out.println("Je suis un animal.");
 	}
 	
+	//prcédure qui affiche les infos de l'animal
 	public void AfficherInfo(){
 		System.out.println("Nom: "+nom+", Âge: "+age+" ans");
 	}
